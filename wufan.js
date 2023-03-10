b95ff686af4f51d74ae9fff64f536802f1e1c72e
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-10 10:28:26 +0800

    Create wufan.js

---
+++ wufan.js
@@ -0,0 +1,18 @@
+var obj = JSON.parse($response.body); 
+
+obj ={
+  "error": 0,
+  "data": {
+    "error_msg": "",
+    "is_success": true,
+    "user_info": {
+      "is_vip": "9A0684792021D73BE42B71491469ADFA",
+      "expired_time": 0,
+      "member_state": 2
+    },
+    "error_code": "",
+    "data_info": []
+  }
+}
+
+$done({body:JSON.stringify(obj)});
