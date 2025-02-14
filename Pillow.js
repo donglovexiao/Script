0624127d9615e0bc9ecbcd1537cdcb891b21b7cd
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2025-02-14 19:17:54 +0800

    Update Pillow.js

--- Pillow.js
+++ Pillow.js
@@ -1,1 +1,156 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('\'6h 6d\';6k(67(p,a,c,k,e,r){e=67(c){66(c<a?"":e(6f(c/a)))+((c=c%a)>35?6c.6e(c+29):c.6g(36))};68(!"".6b(/^/,6c)){6a(;c--;){r[e(c)]=k[c]||e(c)}k=[67(69){66 r[69]}];e=67(){66"\\\\w+"};c=1}6a(;c--;){68(k[c]){p=p.6b(6i 6l("\\\\b"+e(c)+"\\\\b","g"),k[c])}}66 p}(\'4A(4t(p,a,c,k,e,d){e=4t(c){4s(c<a?"":e(4C(c/a)))+((c=c%a)>35?4x.4E(c+29):c.4B(36))};4w(!\\\'\\\'.4v(/^/,4x)){4u(c--)d[e(c)]=k[c]||e(c);k=[4t(e){4s d[e]}];e=4t(){4s\\\'\\\\\\\\w+\\\'};c=1};4u(c--)4w(k[c])p=p.4v(4z 4y(\\\'\\\\\\\\b\\\'+e(c)+\\\'\\\\\\\\b\\\',\\\'g\\\'),k[c]);4s p}(\\\'\\\\\\\'37 3m\\\\\\\';2X(2O(p,a,c,k,e,r){e=2O(c){2N(c<a?"":e(2Z(c/a)))+((c=c%a)>35?2R.2U(c+29):c.2V(36))};2Q(!"".2P(/^/,2R)){2W(;c--;){r[e(c)]=k[c]||e(c)}k=[2O(2Y){2N r[2Y]}];e=2O(){2N"\\\\\\\\\\\\\\\\w+"};c=1}2W(;c--;){2Q(k[c]){p=p.2P(30 31("\\\\\\\\\\\\\\\\b"+e(c)+"\\\\\\\\\\\\\\\\b","g"),k[c])}}2N p}(\\\\\\\'1p(1k(p,a,c,k,e,d){e=1k(c){1j(c<a?"":e(1M(c/a)))+((c=c%a)>1Q?1o.1P(c+29):c.1O(1N))};1l(!\\\\\\\\\\\\\\\'\\\\\\\\\\\\\\\'.1m(/^/,1o)){1n(c--)d[e(c)]=k[c]||e(c);k=[1k(e){1j d[e]}];e=1k(){1j\\\\\\\\\\\\\\\'\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\w+\\\\\\\\\\\\\\\'};c=1};1n(c--)1l(k[c])p=p.1m(1L 1K(\\\\\\\\\\\\\\\'\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\b\\\\\\\\\\\\\\\'+e(c)+\\\\\\\\\\\\\\\'\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\b\\\\\\\\\\\\\\\',\\\\\\\\\\\\\\\'g\\\\\\\\\\\\\\\'),k[c]);1j p}(\\\\\\\\\\\\\\\'d 0=q.1i((D $b!="r"&&$b.0)||1c);d 1=$9&&$9.1;d 2={};B C=$9.7;8(/^c:\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/K\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\.y\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\.4\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/F\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/(J|L)?/.P(C)){8(D $b=="r"){n 1["x-y-H"];n 1["X-G-R"];2.1=1}I 8(0&&0.6&&1["X-M-N-O"]=="4.e.Q"){B a={14:"p-o-m:15:j",T:"p-o-m:15:j",1g:"1f-k-1e:k:1d",S:"1b",1a:"18",};0.6.1h["4.e.h.3.g.f"]=a;0.6.l.3=a;0.6.l.3.16="4.e.h.3.g.f";2.0=q.17(0);$13("11 10\\\\\\\\3l","","Z!Y\\\\\\\\3k",{"W-7":"c://i.E.A/5-z/5/w/12/19/v/u/12/t.s","V-7":"c://i.E.A/5-z/5/w/12/19/v/u/12/t.s",})}}$U(2);\\\\\\\\\\\\\\\',1S,20,\\\\\\\\\\\\\\\'1T|1U|1V|1W|1X|1Y|1Z|1I|1l|1R|1H|1q|1r|1s|22|1t|1u|1v||1w|1x|1G|1z|1y|1A|1B|1C|1D|1E|1F|1J|21|2i||23|2u|2v|2w|2D|2x|2y|2z|2A|2B|2C|2E|2F|2G|2H|2I|2J|2K|2L|2M|2s|2t|2q|2e|24||25|26|27|28||2a|2b||2c|2d|2f||2p|2g|2h|2r|2j|2k|2l|2m|2n\\\\\\\\\\\\\\\'.2o(\\\\\\\\\\\\\\\'|\\\\\\\\\\\\\\\'),0,{}))\\\\\\\',33,3n,"|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||2N|2O|2Q|2P|3F|2R|2X|3o|3p|3q|3r|3s|3t|3u|3v|3w|3x|3y|3z|3A|3i|3C|3j|3D|3h|39|34|31|30|2Z|36|2V|2U|35|3g|33|38|3a|3b|3c|3d|3e|3f|3B|3E|40|3G|4a|\\\\\\\\4g\\\\\\\\47\\\\\\\\48\\\\\\\\2T\\\\\\\\2S|49|\\\\\\\\4b\\\\\\\\4c\\\\\\\\2T\\\\\\\\2S|4d||4e|4f|4i|4h|4q|4p|4o|4r|4m|4l|4j|4k|4n|45|32|46|43|42|3H|3I|3J|3K|3L|3M|3N|3O|3P|3Q|3R|3S|3T|3U|3V|3W|44|3X|41|3Z|3Y".32("|"),0,{}));\\\',62,4Z,\\\'|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||4s|4t|4v|4w|4x|50|51|4E|4B|52|4A|53|4C|4z|4y|4D|62|54|||5g|55|57|5f|58|59|5a|4X|5c|5d|5e|4Y|56|4W|4R|4G|4H|4I|5h|4J|4K|4L|4M|4V|4N|4O|4P|4S|4U|4F|4T|4Q|5b|5F|4u|5i|5K|5L|5M|5N|5O|5P|5V|5Q|5R|5S|5T|5U|64|5W|5X|5Y|5Z|60|61|63|65|5I|5J|5G|5t|5j|5k|5l|5m|5n|5o|5p|5q|5r|5s|5u|5H|5v|5w|5x|5y|5z|5A|5B|5C|5D|5E\\\'.4D(\\\'|\\\'),0,{}))\',62,6q,"||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||66|67|6F|6b|68|6c|6l|6i|6k|6g|6f|6j|6e|6E|6d|6G|6H|6I|6J|6K|6L|6M|6N|6O|6U|6Q|6R|6S|6D|6V|6T|6C|6u|6n|6o|6p|6a|69|6r|6t|6A|6B|6v|6w|6x|6y|6z|6m|6s|6P|6h|7f|6W|7o|7p|7y|7q|7r|7s|7t|7u|7v|7w|7x|7J|7z|7A|7B|7C|7D|7E|7F|7G|7I|7H|7m|7n|7k|77|6X|6Y|6Z|70|71|72|73|74|75|76|78|7j|79|7a|7b|7c|7d|7e|7l||7g|7h|7i".6j("|"),0,{}));',62,480,'||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||return|function|if|inPropName|for|replace|String|strict|fromCharCode|parseInt|toString|use|new|split|eval|RegExp|request|276|u529f|u6210|378|06|subscriberData|body|undefined|obj|premium|com|entitlements|subscriber|103718184_p0|url|img|2023|JSON|while|173|response|var|specialoffer|year|pillow|02|delete|24T17|headers|uff01|09|81|u3002|png|10Z|revenuecat|done|store|purchase_date|original|re|let|typeof|v1|RevenueCat|etag|02Z|else|pixiv|api|receipts|Client|ID|ETag|https|neybox|subscribers|test|requestUrl|stringify|Pillow|00|Bundle|ownership_type|u89e3|Nice|open|u6267|u884c|XiaoMao_Pillow|notify|original_purchase_date|parse|u9501|product_identifier|2222|expires_date|02T02|2022|subscriptions|app_store|PURCHASED|null|media|u5df2'.split('|'),0,{}))
+const Q = {};
+const Q1 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  Q.headers = $request.headers;
+} else if (Q1 && Q1.subscriber) {
+  Q1.subscriber.subscriptions = Q1.subscriber.subscriptions || {};
+  Q1.subscriber.entitlements = Q1.subscriber.entitlements || {};
+  var headers = {};
+  for (var key in $request.headers) {
+  const reg = /^[a-z]+$/;
+  if (key === "User-Agent" && !reg.test(key)) {
+    var lowerkey = key.toLowerCase();
+    $request.headers[lowerkey] = $request.headers[key];
+    delete $request.headers[key];
+    }
+  }
+  var UA = $request.headers['user-agent'];
+  const app = '1';
+  const UAMappings = {
+    'Barcodes':{name:'Unlimited',id:'com.barcodesapp.lifetime'},//12.10
+    'Relax':{name:'pro',id:'com.happydogteam.relax.lifetimePro'},//12.1
+    'Nightcam':{name:'nightcam_pro',id:'com.ahmetserdarkaradeniz.nightcamyearlyalternative'},//11.26
+    'Jellycuts':{name:'pro',id:'standart'},//11.21
+    'Finale%E2%80%A2Pad':{name:'Pro',id:'com.cherpake.finale.lt'},//11.16
+    'outside':{name:'Outside Pro',id:'outside_sub_yearly_super_cheap_free_trial'},//11.16
+    'Sibelius%E2%80%A2Pad':{name:'Premium',id:'com.cherpake.musicpad.all'},//11.16
+    'NumPad':{name:'Pro',id:'com.cherpake.numpad.pro.discount'},//11.16
+    'Drive':{name:'Pro',id:'com.cherpake.drive.viewer.free'},//11.16
+    'TV%20Remote':{name:'Pro',id:'com.cherpake.tvr.all.discount'},//11.16
+    'Remote':{name:'Pro',id:'com.cherpake.macrc.more'},//11.16
+    'Chatty':{name:'pro AI Pro',id:'chatty.yearly.1'},//11.15
+    'ainotes':{name:'HoneyNote AI Pro',id:'NCIAP_A_149_99'},//11.14
+    'quitnow':{name:'pro_features',id:'pro_features_year_subscription'},//11.12
+    'ChatPub':{name:'Unlimited Access',id:'conversationai.year'},//11.11
+    'Unfold':{name:'FF2_STORY',id:'UNFOLD_PRO_YEARLY'},//11.2
+    'Origami':{name:'Premium',id:'origami_499_1m'},//11.1
+    'Reader':{name:'standard',id:'vd_monthly_999'},//10.27
+    'Vocai-iOS':{name:'AI Pro',id:'vocabAI_900_1m'},//10.27
+    'Treering':{name:'Pro',id:'Treering.pro.yearly1'},//10.26
+    'AmazingWidget':{name:'pro',id:'moyo_subcription_yearly'},//10.22
+    'dtdVibe':{name:'pro',id:'com.dtd.aroundu.year'},//9.26
+    'DtdVibe':{name:'pro',id:'com.dtd.playlist.premium.subscription.yearr'},//9.26
+    'AdGuard%20Home%20Remote':{name:'aghrpro',id:'adguard.home.remote.pro'},//9.25
+    'Chatme':{name:'premium',id:'chatme_premium_year_discount'},//9.24
+    'Alpenglow':{ name: 'newPro', id: 'ProLifetime'},//9.23
+    'Opal':{ name: 'premium_tier_2', id: 'com.withopal.opal.premiumtier2lifetime'},//9.11
+    'Photoooo':{ name: 'lifetime', id: 'canoe_28_rnb_forever'},//9.18
+    'Baby%20Generator':{ name: 'premium_features', id: 'babygenerator_499_weekly'},//9.9
+    'Boring':{ name: 'pro', id: 'month'},//8.29lifelog
+    'Snipd':{ name: 'premium', id: 'test_snipd_premium_grandfather_1y_4200_trial_2w_v1'},//9.10
+    'Overdue':{ name: 'Pro', id: '1'},//8.29我的物品
+    'iScape':{ name: 'Pro', id: 'Limited_YearlyProAutoRenew'},//8.15
+    'GigaBody':{ name: 'Pro', id: 'GigaBodySubscriptionYear_v1'},//8.21
+    'FunPix':{ name: 'premium', id: 'intro_price_weekly'},//8.15
+    'WiseMate':{ name: 'vip_entitlement', id: 'wisemate.ai.ios.week'},//8.15
+    'Loora':{ name: 'Yearly', id: 'yearly_119_99_no_trial'},//8.14
+    'Browser':{ name: 'pro', id: 'pro_zoomable'},//8.7
+    'becoming':{ name: 'Strength Pro', id: 'strength_membership_monthly'},//练就
+    'Chords':{ name: 'FullUnlock', id: 'cas_full_unlock_yearly_50_off'},//8.1
+    'reader':{ name: 'subscriptions', id: 'com.valo.reader.vip1.forever'},
+    'Gradient':{ name: 'unlimited', id: 'com.tickettothemoon.gradient.unlimited.yearly.small'},//7.23
+    'Python3IDE':{ name: 'pro', id: 'python3ide_six_month'},//7.14
+    'Scale%20Finder':{ name: 'Pro', id: 'sf_2999_1y_1w0'},//7.14
+    'Who%20Stalks':{ name: 'Premium', id: 'ws_999_1m'},//7.14
+    'PrevisShot':{ name: 'VIP', id: 'com.previsshot.previsshot.continuous_subscribe_12month_vip'},//7.9
+    'Super%20AI%20Chat':{ name: 'premium', id: 'chatbot_v4_1999_1y'},
+    'MusicPutty':{ name: 'pro_version', id: 'mp_3599_1y'},//6.24
+    'Linearity':{ name: 'pro', id: 'linearity_curve_pro_yearly_special_offer_trial'},
+    'iplayTV':{ name: 'com.ll.btplayer.12', id: 'com.ll.btplayer.12'},//6.21
+    '%E8%B5%84%E6%BA%90%E6%90%AC%E8%BF%90%E5%A4%A7%E5%B8%88':{ name: 'SaveTikYoutu_common', id: 'LifetimeSubscription'},
+    'DHWaterMarkManager':{ name: 'Vip', id: 'lifetimeVIP_001'},//6.20
+    'Krishna%20VPN%20Plus%20Browser':{ name: 'vpnz-lifetime', id: 'vpnz-lifetime'},//7.7
+    'XBListeningEnglish':{ name: 'enPro', id: 'com.shenming.newconceptvip.year'},
+    'FretTrainer':{ name: 'pro', id: 'frettrainer.sub.yearly.pro'},//5.7
+    '%E9%B2%B8%E8%90%BD%E6%96%87%E6%A1%88':{ name: 'vip', id: 'jl_year'},//2024.5.6
+    'PeachTree':{ name: 'GoldMember', id: 'LifetimeGoldMembership'},//2024.5.5
+    'No%20Fusion':{ name: 'LivePhoto', id: 'com.grey.livephoto.reference.price'},//2024.5.5
+    'mark_cup':{ name: 'premiun', id: '202403180021'},//20.24.5.4
+    'VOX':{ name: 'VOX Premium', id: 'com.coppertino.VoxMobile.AU.Loop1_v8'},//20.24.4.22
+    'PDF%20Viewer':{ name: 'sub.pro', id: 'com.pspdfkit.viewer.sub.pro.yearly'},//2024.3.21
+    'Text%20Workflow':{ name: 'pro', id: 'tw_99_1m'},//2024.3.2
+    'FaceMa':{ name: 'Pro access', id: 'Pro_Lifetime'},//Facemo
+    'MadeYu':{ name: 'pro_plus', id: 'my_549_1m_400'},//
+    'clica':{ name: 'pro', id: 'clica.vip.year'},//
+    'FoJiCam':{ name: 'ProVersionLifeTime', id: 'com.uzero.cn.fojicam.life2'},//2024.4.9
+    'ShellBoxKit':{ name: 'pro', id: 'ShellBoxKit.Lifetime'},//2024.4.9
+    'PicSeedClient':{ name: 'Pro', id: 'com.picseed.sub.pro.monthly'},//7.6
+    'StarDiary':{ name: 'pro', id: 'com.gsdyx.StarDiary.nonConsumable.forever'},
+    'CountDuck':{ name: 'premium', id: 'Lifetime'},
+    'StarFocus':{ name: 'pro', id: 'com.gsdyx.StarFocus.nonConsumable.forever'},
+    'Context_iOS':{ name: 'pro', id: 'ctx_3y_sspai_preorder_angel'},
+    'Vision':{ name: 'promo_3.0', id: 'vis_lifetime_3.0_promo'},
+    'Structured':{ name: 'pro', id: 'today.structured.pro'},
+    'Cookie':{ name: 'allaccess', id: 'app.ft.Bookkeeping.lifetime'},
+    'CountDuck':{ name: 'premium', id: 'Lifetime'},
+    'HTTPBot':{ name: 'Pro', id: 'httpbot_1499_1y_1w0'},
+    'MyPianist':{ name: 'pro', id: 'com.collaparte.mypianist.pro.gift.twelve'},
+    'TouchRetouchBasic':{ name: 'premium', id: 'tr5_yearlysubsc_30_and_20_dlrs'},//年底订阅
+    'Free':{ name: 'pro', id: 'appspree_pro_lifetime'},
+    'Version':{ name: 'pro', id: 'httpbot_1499_1y_1w0'},
+    'wordswag':{ name: 'pro', id: 'Pro_Launch_Monthly'},
+    'BlackBox':{ name: 'plus', id: 'app.filmnoir.appstore.purchases.lifetime'},
+    'LongmaoApp':{ name: 'pro', id: 'douyina_forever_01'},
+    'AnkiPro':{ name: 'Premium', id: 'com.ankipro.app.lifetime'},
+    'AIChat':{ name: 'AI Plus', id: 'aiplus_yearly'},
+    'SmartAIChat':{ name: 'Premium', id: 'sc_3999_1y'},
+    'AIKeyboard':{ name: 'plus_keyboard', id: 'aiplus_keyboard_yearly'},
+    'TextMask':{ name: 'pro', id: 'tm_lifetime'},
+    'MusicMate':{ name: 'premium', id: 'mm_lifetime_68_premium'},
+    'ImagineAI':{ name: 'pro', id: 'artistai.yearly.1'},
+    'VoiceAI':{ name: 'Special Offer', id: 'voiceannualspecial'},
+    'Langster':{ name: 'Premium', id: 'com.langster.universal.lifetime'},
+    'Chat%E7%BB%83%E5%8F%A3%E8%AF%AD':{ name: 'Premium', id: 'com.tech.AiSpeak.All'},
+    'Readle':{ name: 'Premium', id: 'com.hello.german.yearly'},
+    'image_upscaler':{ name: 'pro', id: 'yearly_sub_pro'},
+    'Muse':{ name: 'pro', id: 'monthly_pro_muse'},
+    'Funexpected%20Math':{ name: 'plus', id: 'Plus6Months14DaysTrial'},
+    'cdiary':{ name: 'Premium', id: 'pub.kiya.daymoment.lifetime'},
+    'Sex%20Actions':{ name: 'Premium Plus', id: 'ru.sexactions.subscriptionPlusWeek1'},
+    'Law':{ name: 'vip', id: 'LawVIPOneYear'},
+    'Emoji+%20%F0%9F%98%9':{ name: 'premium', id: 'com.emoji.freemium.subscription.premium'},
+    'universal':{ name: 'Premium', id: 'remotetv.yearly.01'},
+    'HabitKit':{ name: 'Pro', id: 'habitkit_1799_lt'},
+    'windiary':{ name: 'Pro', id: 'windiary_1799_lt'},
+    'Liftbear':{ name: 'Pro', id: 'liftbear_2399_1y'},
+    'Currency':{ name: 'plus', id: 'com.jeffreygrossman.currencyapp.iap.pro.crossgrade'},
+
+    'VSCO':{name:'pro',id:'vscopro_global_5999_annual_7D_free'},
+    'Pillow':{name:'premium',id:'com.neybox.pillow.premium.yearly'}
+    
+    
+    
+    };
+
+  const data = {
+    "expires_date": "2099-12-31T12:00:00Z",
+    "original_purchase_date": "2023-09-01T11:00:00Z",
+    "purchase_date": "2023-09-01T11:00:00Z",
+    "ownership_type": "PURCHASED",
+    "store": "app_store"
+  };
+  for (const i in UAMappings) {
+    if (new RegExp(`^${i}`, 'i').test(UA)) {
+      const { name, id } = UAMappings[i];
+      Q1.subscriber.subscriptions = {};
+      Q1.subscriber.subscriptions[id] = data;
+      Q1.subscriber.entitlements[name] = JSON.parse(JSON.stringify(data));
+      Q1.subscriber.entitlements[name].product_identifier = id;
+      break;
+    }
+  }
+  Q.body = JSON.stringify(Q1);
+}
+$done(Q);
